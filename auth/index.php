7303cecd46366d2605572cc632fd3d60db4f2b81
Kevin Raneri <kevin.raneri@gmail.com>
2016-03-07 16:41:48 -0500

    Added auth system

---
+++ auth/index.php
@@ -0,0 +1,16 @@
+<?php
+$config = include("/res/config.php");
+$mysql = mysqli_connect($config['host'], $config['username'], $config['password'], $config['db']);
+
+$username = mysqli_real_escape_string($mysql, $_GET['username']);
+$password = md5($_GET['password']);
+
+$query = "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password' LIMIT 1;";
+$result = mysqli_query($mysql, $query);
+$rows = mysqli_num_rows($result);
+if ($rows == 0) {
+	echo "0";
+} else {
+	echo "1";
+}
+?>
---
+++ res/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+"host" => "us-cdbr-azure-east-a.cloudapp.net",
+"username" => "b9929f51d26851",
+"password" => "0a56a689",
+"db" => "messager"
+);
+?>
